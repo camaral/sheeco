1016e498cc7a96878187f13b41d81ede1e8a91f0
Caio Amaral <caiobrandaodoamaral@gmail.com>
2015-02-15 21:34:24 +0100

    Unmarshalling Exceptions and Violations
    forgot one file

---
+++ src/main/java/midas/sheeco/exceptions/OriginalCellType.java
@@ -0,0 +1,15 @@
+package midas.sheeco.exceptions;
+
+public enum OriginalCellType
+{
+    BLANK,
+    NUMERIC,
+    STRING,
+    BOOLEAN,
+    DATE,
+    ERROR,
+    FORMULA,
+    NO_VALUE,
+    FULL_ROW,
+    UNKNOWN;
+}
