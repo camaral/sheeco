0aaa64fdc933098551021b4a25116d5ff5e6f91b
Caio Amaral <caiobrandaodoamaral@gmail.com>
2015-02-15 19:48:24 +0100

    Open POI Worksheet

--- src/main/java/midas/sheeco/Sheeco.java
+++ src/main/java/midas/sheeco/Sheeco.java
@@ -1,11 +1,10 @@
 package midas.sheeco;
 
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import org.apache.poi.poifs.filesystem.NPOIFSFileSystem;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.*;
 import java.util.List;
 import java.util.Set;
 
@@ -14,16 +13,41 @@ import java.util.Set;
  */
 public class Sheeco {
 
-    public void fromSpreadsheet(final File file) {
+    private static void close(
+            final NPOIFSFileSystem fsFileSystem) {
         try {
-            fromSpreadsheet(new BufferedInputStream(new FileInputStream(file)));
-        } catch (final FileNotFoundException e) {
+            fsFileSystem.close();
+        } catch (final IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void fromSpreadsheet(final InputStream stream) {
+    public <T> List<T> fromSpreadsheet(final File file, final Class<T> payloadType) {
+        try {
+            return fromSpreadsheet(new BufferedInputStream(new FileInputStream(file)), payloadType);
+        } catch (final FileNotFoundException e) {
+            throw new RuntimeException(String.format("sheeco.serializer.file.cannot.open"));
+        }
+    }
 
+    public <T> List<T> fromSpreadsheet(final InputStream stream, final Class<T> payloadType) {
+        NPOIFSFileSystem fsFileSystem = null;
+        final Workbook wb;
+
+        try {
+            fsFileSystem = new NPOIFSFileSystem(stream);
+            wb = WorkbookFactory.create(fsFileSystem);
+        } catch (final FileNotFoundException e) {
+            throw new RuntimeException(String.format("sheeco.serializer.file.cannot.open"));
+        } catch (final IOException e) {
+            throw new RuntimeException(String.format("sheeco.serializer.file.wrong.format"));
+        } finally {
+            if (fsFileSystem != null) {
+                close(fsFileSystem);
+            }
+        }
+
+        throw new RuntimeException("TODO");
     }
 
     public void toSpreadsheet(
